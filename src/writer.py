c3dbcdbb56e7fac9f34ddbee32124e380c6e03f5
dcsp3 <88645471+dcsp3@users.noreply.github.com>
2021-08-27 22:27:29 +0530

    Update and rename sql_db.py to writer.py

---
+++ src/writer.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+from process_listener import ProcessDetails
+
+
+class Writer:
+    def __init__(self):
+        self.db = sqlite3.connect('activitytracker.sqlite')
+        self.cursor = self.db.cursor()
+
+        try:
+            self.cursor.execute('create table activity(pid int, title varchar(256), exe_name varchar(256), process_start datetime(6), process_end datetime(6))')
+        except sqlite3.OperationalError:
+            pass
+
+    def write(self, process_details: ProcessDetails):
+        vals = (process_details.pid,
+                process_details.title,
+                process_details.exe_name,
+                process_details.process_start,
+                process_details.process_end)
+
+        self.cursor.execute('insert into activity(pid, title, exe_name, process_start, process_end) values (?, ?, ?, ?, ?)', vals)
+        self.db.commit()
+        
