1387231204c47a31c08cd2587029df62aff600da
dtele <72906211+dtele@users.noreply.github.com>
2021-08-28 21:14:37 +0000

    Update tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -3,10 +3,13 @@ import subprocess
 from datetime import datetime, timedelta
 from typing import Any, Callable
 
-from process_listener import Listener, ProcessDetails
+from listener import Listener, ProcessDetails
 
 
 class Icon:
+    """
+    A class to manage icons.
+    """
     def __init__(self):
         try:
             os.mkdir(os.path.join(os.getcwd(), 'icons'))
@@ -14,7 +17,12 @@ class Icon:
             pass
 
     @staticmethod
-    def create(exe_path):
+    def create(exe_path) -> None:
+        """
+        Calls a PowerShell script which extracts icon from the specified executable and saves it as bitmap.
+
+        :param exe_path: path of executable
+        """
         try:
             exe_path = '"' + exe_path + '"'
             process_name = exe_path[exe_path.rfind('\\') + 1:-1].replace('.exe', '.bmp')
@@ -27,29 +35,41 @@ class Icon:
 
 
 class Tracker:
-    def __init__(self, callback_function: Callable[[ProcessDetails], Any] = print):
+    """
+    A class to track time spent on processes.
+    """
+    def __init__(self, callback_function: Callable[[ProcessDetails], Any] = print, delay: int = 1_000):
+        """
+        :param callback_function: function to be called when process is switched
+        :param delay: delay between calls to get process in milliseconds
+        """
         self.callback_function = callback_function
         self.icon_manager = Icon()
 
-        self.process_details_prev = ProcessDetails(0, '', '', '')
-        self.process_start = datetime.now()
+        self.process_details_prev = ProcessDetails()
+        self.process_start = self.process_end = datetime.now()
 
-        self.listener = Listener(callback_function=self.on_activity, delay=10)
-        self.listener.start()
+        self.process_listener = Listener(callback_function=self.check_window, delay=delay)
+        self.process_listener.start()
 
-    def on_activity(self, process_details: ProcessDetails):
+    def check_window(self, process_details: ProcessDetails):
+        """
+        Checks if the active process is the same as the one in last tick.
+        Populates process_start and process_end for process_details_prev and calls the callback function if not.
+
+        :param process_details: ProcessDetails object of the current active process
+        """
         try:
             if not process_details.isnone():
-                self.icon_manager.create(process_details.exe_path)
+                if not self.process_details_prev.isnone() and self.process_details_prev != process_details:
+                    self.icon_manager.create(process_details.exe_path)
 
-                if not self.process_details_prev.isnone():
-                    if process_details != self.process_details_prev:
-                        self.process_end = datetime.now()
-                        self.process_details_prev.process_end, self.process_details_prev.process_start= self.process_end, self.process_start
-                        self.process_details_prev.runtime = self.process_end - self.process_start
-                        self.process_start = datetime.now()
+                    self.process_end = datetime.now()
+                    self.process_details_prev.process_end, self.process_details_prev.process_start = self.process_end, self.process_start
+                    self.process_details_prev.runtime = self.process_end - self.process_start
+                    self.process_start = datetime.now()
 
-                        self.callback_function(self.process_details_prev)
+                    self.callback_function(self.process_details_prev)
 
                 self.process_details_prev = process_details
         except AttributeError:
