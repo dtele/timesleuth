b6077ba4b118b76fc0d64a060f56ed2c247de668
dtele <72906211+dtele@users.noreply.github.com>
2021-09-10 06:46:47 +0000

    reader.py- update docstrings, separate methods

--- src/reader.py
+++ src/reader.py
@@ -10,15 +10,21 @@ class Reader:
     """
     A class to read data from the database.
     """
-    def __init__(self, db_name: str):
+    def __init__(self, db_name):
         """
-        :param db_name: name of database
+        :param db_name: relative path of database
         """
         self.db = sqlite3.connect(db_name)
         self.cursor = self.db.cursor()
 
     @staticmethod
     def to_process_details(row: Tuple[str, str, str, str]) -> ProcessDetails:
+        """
+        Converts SQL entries to ProcessDetails objects.
+
+        :param row: individual row read from sql table
+        :returns: converted ProcessDetails object
+        """
         process = ProcessDetails(title=row[0], exe_name=row[1][row[1].rfind('\\') + 1:], exe_path=row[1])
         process.process_start = datetime.strptime(row[2], '%Y-%m-%d %H:%M:%S.%f')
         process.process_end = datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S.%f')
@@ -27,13 +33,15 @@ class Reader:
         return process
 
     @staticmethod
-    def get_runtime(data: List[ProcessDetails]) -> list[str]:
+    def get_runtime(data: List[ProcessDetails]) -> List[Tuple[str, timedelta]]:
         """
-        Calculates runtime from process start and process end.
-        :param data: process details
-        :returns: runtime of process
+        Calculates total runtime of each unique process.
+
+        :param data: list of ProcessDetails objects converted from sql entries
+        :returns: list of unique processes and their total runtime
         """
         process_runtimes = {}
+
         for elem in data:
             if elem.exe_path in process_runtimes:
                 process_runtimes[elem.exe_path] += elem.runtime
@@ -42,16 +50,16 @@ class Reader:
 
         return sorted(process_runtimes.items(), key=lambda i: i[1], reverse=True)
 
-    def get_info(self, date: datetime.date) -> list[str]:
+    def read_rows(self, date: datetime.date) -> List[ProcessDetails]:
         """
         Reads data from the database.
-        :param date: date from which the data is required
-        :returns: exe_path and runtime of processes
+
+        :param date: lower range of timeframe to read entries from
+        :returns: list of ProcessDetails objects converted from sql table
         """
-        self.cursor.execute(
-            'select title, exe_path, process_start, process_end from activity where process_start > (?)', (date,))
+        self.cursor.execute('select title, exe_path, process_start, process_end from activity where process_start > (?)', (date,))
         output = [Reader.to_process_details(i) for i in self.cursor.fetchall()]
 
         self.db.commit()
 
-        return Reader.get_runtime(output)
+        return output
