bc2c4102f6b2519fe8c1612f96b51f63f323e043
dtele <72906211+dtele@users.noreply.github.com>
2021-12-27 23:26:16 +0000

    add direct .NET calls
    friendship ended with powershell 😠

--- src/icons.py
+++ src/icons.py
@@ -1,8 +1,17 @@
-import base64
+# Python imports
 import io
-import subprocess
 
+import clr
 from PIL import Image
+
+# .NET runtime imports
+clr.AddReference('System.Drawing')
+clr.AddReference('System.IO')
+
+from System.Drawing import Icon as net_Icon
+from System.Drawing import Imaging
+from System.Drawing import Size
+from System.IO import MemoryStream
 
 
 class Icon:
@@ -12,8 +21,12 @@ class Icon:
         self.dominant_color = self.get_dominant_color()
 
     def get_pil_img(self) -> Image:
-        img_base64 = subprocess.run(['powershell', '-executionpolicy', 'bypass', './icons.ps1', '-ExePath', rf'"{self.exe_path}"'], capture_output=True).stdout
-        img_bytes = base64.decodebytes(img_base64)
+        img_bmp = net_Icon(net_Icon.ExtractAssociatedIcon(self.exe_path), Size(64, 64)).ToBitmap()
+        mem_stream = MemoryStream()
+
+        img_bmp.Save(mem_stream, Imaging.ImageFormat.Png)
+        img_bytes = bytearray(mem_stream.ToArray())
+
         img_file_object = io.BytesIO(img_bytes)
         img_pil = Image.open(img_file_object)
 
@@ -22,10 +35,10 @@ class Icon:
     def get_dominant_color(self) -> str:
         img_copy = self.img_pil.copy()
         img_compressed = img_copy.quantize(colors=8, kmeans=3).convert('RGBA')
-        pixels = img_compressed.getcolors(2 ** 8)
-        # remove transparent (a = 0) and black (sum(r, g, b) < 45) and white (sum(r, g, b) > 720) pixels
+        pixels = img_compressed.getcolors(8)
+        # remove transparent (a = 0) and almost black (sum(r, g, b) < 45) and white (sum(r, g, b) < 720) pixels
         pixels = [i for i in pixels if i[1][3] != 0 and 45 < sum(i[1][0:3]) < 720]
         dominant_rgb = max(pixels, key=lambda i: i[0])[1][0:3]
         dominant_hex = '#' + '%02x%02x%02x' % dominant_rgb
-        
+
         return dominant_hex
