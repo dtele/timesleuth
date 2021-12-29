b7ed981cfde6dd21ad15cab9dbfd010f648e6bca
xyltius <77830147+xyltius@users.noreply.github.com>
2021-12-29 05:43:18 +0530

    Added icon exception block and graph legend

--- src/graph.py
+++ src/graph.py
@@ -12,14 +12,21 @@ df=db_obj.read_rows('2021-12-17')
 df['sort_key']=[i.split('\\')[-1][:-4].capitalize() for i in df['exe_path']]
 df=df.sort_values(by='sort_key',inplace=False)
 
-path_icon={i:Icon(i) for i in df['exe_path'].unique()}
+path_icon={}
+for i in df['exe_path'].unique():
+    try:    
+        path_icon[i]=Icon(i)
+    except Exception as e:
+        print(e,f"\n{i} is not a valid path.")
+        pass
+    
 colors=sns.color_palette([i.dominant_color for i in path_icon.values()])
 
 def offset_image(coord, img_obj, ax1_local):
     img=np_arr(img_obj.img_pil)
     im=OffsetImage(img)
     im.image.axes=ax1_local
-    ab=AnnotationBbox(im, (0, coord),  xybox=(-35,0), frameon=False,
+    ab=AnnotationBbox(im, (0, coord),  xybox=(-30,0), frameon=False,
                         xycoords='data',  boxcoords="offset points", pad=0)
     ax1_local.add_artist(ab)
 
@@ -31,13 +38,13 @@ plt.figure(figsize=(10,5))
 
 sns.set_theme(style='ticks',context='paper',font='arial')
 
-ax1=sns.stripplot(data=df,y='exe_path',x='instance_time',palette=colors,linewidth=1.2,size=6.9)
+ax1=sns.stripplot(data=df,y='exe_path',x='instance_time',hue='sort_key',palette=colors,linewidth=1.2,size=6.9)
 ax1.set(xlabel='',ylabel='',xticklabels=[],yticklabels=[])
-#ax1.legend(loc='best', bbox_to_anchor=(1,0.5, 0.5, 0.5),frameon=True,title='Tasks')
+ax1.legend(loc='best', bbox_to_anchor=(1,0.5, 0.5, 0.5),frameon=True,title='Tasks')
 
 ax2=sns.barplot(y='exe_path',x='runtime',data=df,dodge=False,palette=colors,saturation=0.69)
 ax2.set(xlabel='Runtime',ylabel='',yticklabels=[])
-ax2.set_yticklabels([i for i in df['sort_key'].unique()], va='center', rotation=90)
+#ax2.set_yticklabels([i for i in df['sort_key'].unique()], va='center', rotation=90)
 
 ax2.xaxis.set_major_formatter(FuncFormatter(label_to_hours))
 ax2.xaxis.set_major_locator(MultipleLocator(60 if df['runtime'].max()>60 else 30))
